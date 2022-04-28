b9a374f903cf274a92dab60c87e3ec4cbb5ee0ca
dnfebri <dwinofebri@gmail.com>
2022-04-28 23:00:50 +0700

    video 7 Route Configurasi

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,37 +1,37 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import Vue from 'vue';
+// import VueRouter from 'vue-router'
+// Vue.use(VueRouter)
 
 Vue.component('header-component', require('./components/header.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('footer-component', require('./components/footer.vue').default);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const Home = require('./view/Home.vue')
+const About = require('./view/About.vue');
+const { default: VueRouter } = require('vue-router');
+
+const routes = [
+    {
+        path: '',
+        componen: Home
+    },
+    {
+        path: '/about',
+        componen: About
+    },
+]
+
+const route = new VueRouter({
+    routes
+})
 
 const app = new Vue({
     el: '#app',
     data: {
         title: 'Bljr Vue Laravel'
-    }
+    },
+    route
 });
